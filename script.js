799278d3dd3bcad5ea9c0ceb3541ff3be7615468
Iamtejas <tejasbhajbhuje.dev@gmail.com>
2025-09-25 10:15:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+ const container = document.querySelector('.container');
+    const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+    const SQUARES = 800;
+
+    for (let i = 0; i < SQUARES; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+
+      square.addEventListener('mouseover', () => setColor(square));
+      square.addEventListener('mouseout', () => removeColor(square));
+
+      container.appendChild(square);
+    }
+
+    function setColor(element) {
+      const color = getRandomColor();
+      element.style.backgroundColor = color;
+    }
+
+    function removeColor(element) {
+      setTimeout(() => {
+        element.style.backgroundColor = '#1d1d1d';
+      }, 1000); // 1 second
+    }
+
+    function getRandomColor() {
+      return colors[Math.floor(Math.random() * colors.length)];
+    }
